0f3d269e5eb31a8b8157b5d22c7d04fa306c5506
Arnaud Dufranne <a.dufranne@criteo.com>
2019-06-10 14:22:36 +0200

    Migrating to IOApp

--- src/main/scala/process/examples/BasicProcess.scala
+++ src/main/scala/process/examples/BasicProcess.scala
@@ -11,16 +11,37 @@ import doodle.java2d._
 import doodle.java2d.effect.Frame
 import scala.util.Random
 
+object Numbers {
+  val randomDouble: IO[Double] = IO { Random.nextDouble() }
+  val nextGaussian: IO[Double] = IO { Random.nextGaussian() }
+  val randomColor: IO[Color] = (randomDouble, randomDouble, randomDouble).tupled.map({
+    case (r1, r2, r3) =>
+      Color.hsla(
+        (r1 / 3.0 - 0.33).turns, // blues and reds
+        r2 / 2.0 + 0.4, // fairly saturated
+        r3 / 2.0 + 0.4, // fairly light
+        0.7 // Somewhat transparent
+      )
+  })
+}
+
 /** Simple process with no interaction between the elements. */
-object BasicProcess extends App {
-  /** How much we change the location by when we step forward. */
-  val locationDelta = 5.0
-  /** How much we change the heading by when we turn. */
-  val headingDelta = 30.degrees
+object BasicProcess extends IOApp {
+
+  import Numbers._
+
+  object State {
+    /** How much we change the location by when we step forward. */
+    val locationDelta = 5.0
+
+    /** How much we change the heading by when we turn. */
+    val headingDelta = 30.degrees
+    val initial = State(Point.zero, Angle.zero, List.empty)
+  }
 
   final case class State(location: Point, heading: Angle, path: List[Point]) {
     def forward: State = {
-      val newLocation = location + Vec.polar(locationDelta, heading)
+      val newLocation = location + Vec.polar(State.locationDelta, heading)
       this.copy(
         location = newLocation,
         path = newLocation +: path
@@ -28,88 +49,66 @@ object BasicProcess extends App {
     }
 
     def clockwise: State =
-      this.copy(heading = heading + headingDelta)
+      this.copy(heading = heading + State.headingDelta)
 
     def anticlockwise: State =
-      this.copy(heading = heading - headingDelta)
+      this.copy(heading = heading - State.headingDelta)
 
     def toImage: Image =
       Image.interpolatingSpline(path)
   }
-  object State {
-    val initial = State(Point.zero, Angle.zero, List.empty)
-  }
 
   type Event = Double
 
-  /** The finite state machine defines how the state evolves over time. Tweaking
-    * the probabilities will arrive at different results. */
-  val fsm =
-    Fsm[State,Event]{(state, choice) =>
-      if(choice < 0.5) state.forward
-      else if (choice < 0.75) state.clockwise
-      else state.anticlockwise
-    }
+  override def run(args: List[String]): IO[ExitCode] = {
+    /** The finite state machine defines how the state evolves over time. Tweaking
+      * the probabilities will arrive at different results. */
+    val fsm =
+      Fsm[State,Event]{ (state, choice) =>
+        if(choice < 0.5) state.forward
+        else if (choice < 0.75) state.clockwise
+        else state.anticlockwise
+      }
 
-  /** Execute one step of the FSM */
-  def step(state: State): IO[State] = randomDouble.map(r => fsm(state, r))
+    /** Execute one step of the FSM */
+    def step(state: State): IO[State] = randomDouble.map(r => fsm(state, r))
 
-  /** Execute count steps of the FSM */
-  def iterate(count: Int, state: State): IO[State] = {
-    if(count == 0) IO.pure(state)
-    else {
-      step(state).flatMap( s => iterate(count - 1, s))
-    }
+    /** Execute count steps of the FSM */
+    def iterate(count: Int, state: State): IO[State] =
+      if(count == 0) IO.pure(state)
+      else
+        step(state).flatMap( s => iterate(count - 1, s))
+
+    def squiggle(initialState: State): IO[Image] =
+      randomColor.flatMap(c =>
+        iterate(100, initialState)
+          .map(s => s.toImage.strokeWidth(3.0).strokeColor(c))
+      )
+
+    val initialPosition: IO[Point] = (nextGaussian, nextGaussian).tupled.map({
+      case (r1, r2) =>
+        // Poisson disk sampling might be more attractive
+        Point(r1 * 150, r2 * 150)
+    })
+
+    def initialDirection(position: Point): Angle =
+      (position - Point.zero).angle
+
+    val squiggles: IO[Image] =
+      (0 to 500).toList.traverse {_ =>
+        val a: IO[Image] = for {
+          pt <- initialPosition
+          angle = initialDirection(pt)
+          state = State(pt, angle, List.empty)
+          r <- squiggle(state)
+        } yield r
+        a
+      }.map(_.allOn)
+
+    val frame = Frame.fitToPicture().background(Color.black)
+    val go: IO[Unit] = squiggles.map(_.draw(frame))
+
+    go.map(_ => ExitCode.Success)
   }
 
-  private def randomDouble: IO[Double] = IO {
-    Random.nextDouble()
-  }
-
-  def randomColor(): IO[Color] = for {
-    r1 <- randomDouble
-    r2 <- randomDouble
-    r3 <- randomDouble
-  } yield
-    Color.hsla(
-      (r1 / 3.0 - 0.33).turns, // blues and reds
-      r2 / 2.0 + 0.4, // fairly saturated
-      r3 / 2.0 + 0.4, // fairly light
-      0.7 // Somewhat transparent
-    )
-
-  def squiggle(initialState: State): IO[Image] =
-    randomColor().flatMap(c => 
-      iterate(100, initialState)
-        .map(s => s.toImage.strokeWidth(3.0).strokeColor(c))
-    )
-    
-  private def nextGaussian: IO[Double] = IO { Random.nextGaussian() }
-  def initialPosition(): IO[Point] = for {
-    r1 <- nextGaussian
-    r2 <- nextGaussian
-    // Poisson disk sampling might be more attractive
-  } yield Point(r1 * 150, r2 * 150)
-
-  def initialDirection(position: Point): Angle =
-    (position - Point.zero).angle
-
-  def squiggles(): IO[Image] = {
-    val result: IO[List[Image]] = (0 to 500).toList.traverse {_ =>
-      val a: IO[Image] = for { 
-        pt <- initialPosition()
-        angle = initialDirection(pt)
-        state = State(pt, angle, List.empty)
-        r <- squiggle(state)
-      } yield r
-      a
-    }
-
-    result.map(_.allOn)
-  }
-
-  val frame = Frame.fitToPicture().background(Color.black)
-  def go() = squiggles().map(_.draw(frame))
-
-  go().unsafeRunSync()
 }
